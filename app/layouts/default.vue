0fb06fd5800084a5acbaae262d83726f3c004306
HoudaifaBS <boucena214@gmail.com>
2026-09-04 23:25:02 +0100

    feat: add Tailwind CSS support and scaffold initial application layouts and pages

---
+++ app/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/board">Board</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <slot />
+        </main>
+        <footer>
+            <p>Copyright © 2026 Task Manager App. All rights reserved.</p>
+        </footer>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-3xl underline text-red-500">Home</h1>
+    </div>
+</template>
